16b1ba80249d6ca4b34a8b8dc906e1b55ef02c7a
Brad Ross <bross35@stanford.edu>
2025-10-23 17:05:04 -0700

    fixed typos in get_io_paths.R

--- code/get_io_paths.R
+++ code/get_io_paths.R
@@ -1,4 +1,15 @@
 DATA_PATH <- Sys.getenv("DATA_PATH")
+
+RAW_DATA_PATH <- file.path(DATA_PATH, "raw_data")
+if (!dir.exists(RAW_DATA_PATH)) {
+  dir.create(RAW_DATA_PATH, recursive = TRUE)
+}
+
+CLEAN_DATA_PATH <- file.path(DATA_PATH, "clean_data")
+if (!dir.exists(CLEAN_DATA_PATH)) {
+  dir.create(CLEAN_DATA_PATH, recursive = TRUE)
+}
+
 PROCESSED_DATA_PATH = file.path(DATA_PATH, "processed_data")
 if (!dir.exists(PROCESSED_DATA_PATH)) {
   dir.create(PROCESSED_DATA_PATH, recursive = TRUE)
@@ -8,4 +19,3 @@ FIGURES_PATH = file.path(Sys.getenv("OUTPUT_PATH"), "figures")
 if (!dir.exists(FIGURES_PATH)) {
   dir.create(FIGURES_PATH, recursive = TRUE)
 }
-
