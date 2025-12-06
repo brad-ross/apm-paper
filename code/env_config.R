d997ee14047dec3d69b7802210ae83b8ac2e8f60
Brad Ross <bross35@stanford.edu>
2025-12-06 12:38:28 -0800

    moved environment variable configuration code to separate file

---
+++ code/env_config.R
@@ -0,0 +1,24 @@
+# Shared environment configuration for replication scripts
+#
+# This file centralizes environment variable parsing used across multiple scripts.
+# Source this file to get access to common configuration variables.
+
+# Year range configuration
+FIRST_YEAR <- as.integer(Sys.getenv("FIRST_YEAR"))
+LAST_YEAR <- as.integer(Sys.getenv("LAST_YEAR"))
+
+# Model configuration
+MAX_RANK <- as.integer(Sys.getenv("MAX_RANK"))
+YEAR_CLUSTER_SIZE <- as.integer(ceiling((LAST_YEAR - FIRST_YEAR) / MAX_RANK))
+MIN_COHORT_SIZE <- as.integer(Sys.getenv("MIN_COHORT_SIZE"))
+
+# Clustering configuration
+MIN_K <- as.integer(Sys.getenv("MIN_K"))
+MAX_K <- as.integer(Sys.getenv("MAX_K"))
+CHOSEN_K <- as.integer(Sys.getenv("CHOSEN_K"))
+
+# Threading configuration
+num_threads <- as.integer(Sys.getenv("APM_MAX_THREADS"))
+if (is.na(num_threads)) {
+    num_threads <- NULL
+}
